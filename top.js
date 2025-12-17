8b881ad4743426ba4c584de3a8379007a0b4578a
Aadarshag7 <aadarshgautam40@gmail.com>
2025-12-17 19:43:18 +0545

    MODIfy

--- top.js
+++ top.js
@@ -1,6 +1,22 @@
 let tops=
 document.getElementById("top");
-tops.addEventListener("submit",e=>{
+let sops=
+document.getElementById("pot");
+tops.addEventListener("submit",function(e){
     e.preventDefault();
-    console.log("formSubmitted");
+ let name=
+ sops.value;
+  fetch("/submit",{
+   method:"POST",
+   headers:{
+    "content-type":"application/json"
+   } ,
+   body:JSON.stringify({name:name})
+})
+.then(res=>res.json())
+.then(data=>{
+    console.log(data);
+});
+
+    
 });
